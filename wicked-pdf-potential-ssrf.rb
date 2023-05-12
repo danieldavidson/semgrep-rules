d05902651e9e2cc05d02f4b142b884d4de3af3b1
Daniel <5460596+danieldavidson@users.noreply.github.com>
2023-05-12 13:46:12 -0400

    Merge rules into single file

--- wicked-pdf-potential-ssrf.rb
+++ wicked-pdf-potential-ssrf.rb
@@ -1,3 +1,32 @@
+respond_to do |format|
+  format.html
+  format.pdf do
+    render pdf: "file_name"
+  end
+end
+
+respond_to do |format|
+  format.pdf do
+    render :pdf => "my_pdf_name.pdf",
+      :disposition => "inline",
+      :template => "controller_name/show.pdf.erb",
+      :layout => "pdf_layout.html"
+  end 
+
+  format.html
+end
+
+respond_to do |format|
+  format.html
+  format.pdf do
+  render pdf: [@post.id, @post.name].join('-'),
+    template: "posts/show.html.erb",
+    formats: [:html],
+    disposition: :inline,
+    layout: 'pdf'
+  end
+end
+
 # create a pdf from a string
 pdf = WickedPdf.new.pdf_from_string('<h1>Hello There!</h1>')
 
